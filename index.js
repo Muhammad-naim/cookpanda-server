e05c6a5b63ab4b9ad680db604cb2ce4230d958a4
naim <naimulislam0927@gmail.com>
2023-05-02 09:22:12 +0600

    added foodItems data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require('cors')
 const app = express()
 const port = 5000
 const chefData = require('./data/chefs.json')
-
+const itemsData = require('./data/foodItems.json')
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -17,7 +17,14 @@ app.get('/chefs/:id', (req, res) => {
     const chef = chefData.find(item => item.id == id)
     res.send(chef)
 })
-
+app.get('/items',(req, res) => {
+    res.send(itemsData)
+})
+app.get('/items/:id', (req, res) => {
+    const id = req.params.id;
+    const item = itemsData.find(item => item.id == id)
+    res.send(item)
+})
 
 app.listen(port, () => {
     console.log(`port is running on port ${port}`);
