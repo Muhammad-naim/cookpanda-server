cc342c36995f1678e3f4aba413f60340557d2c39
naim <naimulislam0927@gmail.com>
2023-05-02 06:16:35 +0600

    added chef's data

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const port = 5000
+const chefData = require('./data/chefs.json')
+
 
 app.use(cors())
-
 app.get('/', (req, res) => {
-    res.send('cookpanda is running')
+    res.send('server is running')
 })
+app.get('/chefs', (req, res) => {
+    res.send(chefData)
+})
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const chef = chefData.find(item => item.id == id)
+    res.send(chef)
+})
+
 
 app.listen(port, () => {
     console.log(`port is running on port ${port}`);
